797173ebce91c037a665d5a0bf4dabf955118f7c
Saee <saee_saadat@outlook.com>
2023-04-05 23:47:59 +0330

    backend: - minor changes don't know what

--- Backend/app.py
+++ Backend/app.py
@@ -1,6 +1,8 @@
 from typing import List, Dict, Tuple
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
+from illegal_word_finder import run as run_illegal_finder
 
 
 class InputSchema(BaseModel):
@@ -13,13 +15,33 @@ class OutputSchema(BaseModel):
 
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
-@app.post("/run")
-async def run(inp: InputSchema) -> OutputSchema:
+@app.post("/runmock")
+async def run_mock(inp: InputSchema) -> OutputSchema:
     out = {
         "تفنگ": [(0, 4)],
         "سرکه": [(6, 14)],
         "اصغر": [(18, 25)]
     }
     return OutputSchema(illegals=out)
+
+
+@app.post("/run")
+async def run(inp: InputSchema) -> OutputSchema:
+    res = run_illegal_finder(inp.text, inp.illegal_words)
+    return OutputSchema(illegals=res)
+
+
+@app.get('/test')
+async def test():
+    return {
+        "message": "OK!"
+    }
--- Backend/illegal_word_finder.py
+++ Backend/illegal_word_finder.py
@@ -3,9 +3,10 @@ import hazm
 from strsimpy.weighted_levenshtein import WeightedLevenshtein
 import regex
 import re
+import yaml
 
 ILLEGAL_EDIT_DISTANCE_THRESHOLD = 3
-ACCEPTABLE_PERSIAN_CHARS_REGEX = r'[^\u0621-\u064A|\u0686|\u0698|\u06A9|\u06af|\u06be|\u06c1|\u06c3]'
+NON_PERSIAN_CHARS_REGEX = r'[^\u0621-\u064A|\u0686|\u0698|\u06A9|\u06af|\u06be|\u06c1|\u06c3]'
 
 
 def get_persian_words_dictionary():
@@ -27,14 +28,27 @@ def get_persian_words_dictionary():
 
     return persian_words
 
-def get_persian_similar_characters():
-    # with open()
-    pass
 
-def hazm_normalize(raw_string):
+def get_persian_similar_characters():
+    with open("assets/similar_persian_chars.yml", 'r') as f:
+        chars_lists = yaml.safe_load(f)
+        indices = {}
+
+        for i, group in enumerate(chars_lists):
+            for city in group:
+                indices.setdefault(city, set()).add(i)
+
+
+def common_groups(city1, city2, indices: dict):
+    return indices.get(city1, set()) & indices.get(city2, set())
+
+
+def hazm_normalize(word_list):
     normalizer = hazm.Normalizer()
-    normal_string = normalizer.normalize(raw_string)
-    return normal_string
+    normal_word_list = []
+    for word in word_list:
+        normal_word_list.append((normalizer.normalize(word[0]), word[1]))
+    return normal_word_list
 
 
 def tokenize(normal_string):
@@ -59,7 +73,7 @@ def get_illegal_regex_from_words(illegal_words_list: list[str]) -> list[str]:
     return word_regex_list
 
 
-def detect_bad_formed_words(word_list: list[str], illegal_words: list[str]) -> list[(str, str, (int, int))]:
+def detect_bad_formed_words(word_list: list, illegal_words: list) -> list:  # [(str, str, (int, int))]:
     dubiouses = []  # format --> [(illegal, found, (start, end))]
     illegal_regexes = get_illegal_regex_from_words(illegal_words)
 
@@ -78,20 +92,20 @@ def detect_bad_formed_words(word_list: list[str], illegal_words: list[str]) -> l
 
 def edit_distance(s1, s2):
     def insertion_cost(char):
-        if re.match(ACCEPTABLE_PERSIAN_CHARS_REGEX, char):
+        if not re.match(NON_PERSIAN_CHARS_REGEX, char):
             return 2.0
         return 0.1
 
     def deletion_cost(char):
-        if re.match(ACCEPTABLE_PERSIAN_CHARS_REGEX, char):
+        if not re.match(NON_PERSIAN_CHARS_REGEX, char):
             return 2.0
         return 0.1
 
     def substitution_cost(char_a, char_b):
         cost = 0.1
-        if re.match(ACCEPTABLE_PERSIAN_CHARS_REGEX, char_a):
+        if re.match(NON_PERSIAN_CHARS_REGEX, char_a):
             cost += 2.0
-        if re.match(ACCEPTABLE_PERSIAN_CHARS_REGEX, char_b):
+        if re.match(NON_PERSIAN_CHARS_REGEX, char_b):
             cost += 2.0
         return cost
 
@@ -103,33 +117,82 @@ def edit_distance(s1, s2):
     return weighted_levenshtein.distance(s1, s2)
 
 
-def is_false_positive(persian_dictionary: dict, token: str, illegal_word: str) -> bool:
+def is_false_positive(token: str, illegal_word: str, persian_words: set) -> bool:
     # check if word exists in the persian dictionary
-    if token not in persian_dictionary and \
+    if token not in persian_words and \
             edit_distance(token, illegal_word) < ILLEGAL_EDIT_DISTANCE_THRESHOLD:
+        print("True positive: ", token)
         return False  # not false positive
 
     return True
 
 
-def clean_false_positives(dubiouses: List[str], persian_dictionary: dict):
-    return [dubious for dubious in dubiouses if is_false_positive(persian_dictionary, dubious[1], dubious[0])]
+def clean_false_positives(dubiouses: List[str], persian_dictionary: set):
+    return [dubious for dubious in dubiouses if is_false_positive(dubious[1], dubious[0], persian_dictionary)]
 
 
 def dubiouses_to_output(dubiouses):
     output = {}  # format --> {'illegal':[(start, end)]}
     for dubious in dubiouses:
+        # todo --> make set
         output.setdefault(dubious[0], []).append(dubious[2])
     return output
 
 
+def make_integrated_string(input_string):
+    integrated_string = ''
+    integrated_spans = []
+    counter = 0
+    for char in input_string:
+        if re.match(r'[\u0621-\u064A|\u0686|\u0698|\u06A9|\u06af|\u06be|\u06c1|\u06c3|\u06CC]', char):
+            integrated_string += char
+            integrated_spans.append(counter)
+            counter += 1
+        else:
+            counter += 1
+    return integrated_string, integrated_spans
+
+
+def detect_bad_formed_integrated(integrated_string: str, integrated_spans, illegal_words: list[str]) -> list[
+    (str, str, (int, int))]:
+    dubiouses = []  # format --> [(illegal, found, (start, end))]
+    illegal_regexes = get_illegal_regex_from_words(illegal_words)
+
+    # Combine regexes
+    regex_combination = "(" + ")|(".join(illegal_regexes) + ")"
+
+    bad_word_match = re.search(regex_combination, integrated_string)
+    if bad_word_match:
+        captured_groups = bad_word_match.groups()
+        for i, group in enumerate(captured_groups):
+            if group:
+                span = bad_word_match.span(i + 1)
+                dubiouses.append((illegal_words[i], '', (integrated_spans[span[0]], integrated_spans[span[1]])))
+    return dubiouses
+
+
+def get_illegal_regex_for_integrated(illegal_words_list: list[str]) -> list[str]:
+    word_regex_list = []
+    for illegal in illegal_words_list:
+        word_regex = '+'.join(illegal) + '+'
+        word_regex_list.append(word_regex)
+    return word_regex_list
+
+
+# def clean_fp_integrated(dubiouses):
+#     return [dubious for dubious in dubiouses if is_false_positive(dubious[1], dubious[0])]
+
+
 def run(text: str, illegal_words: List[str]):
     persian_dictionary = get_persian_words_dictionary()
-    normal_input = hazm_normalize(text)
-    word_list = tokenize(normal_input)
-    dubiouses = detect_bad_formed_words(word_list, illegal_words)
+    word_list = tokenize(text)
+    normal_word_list = hazm_normalize(word_list)
+    dubiouses = detect_bad_formed_words(normal_word_list, illegal_words)
     dubiouses = clean_false_positives(dubiouses, persian_dictionary)
-    output = dubiouses_to_output(dubiouses)
+    integrated_string, integrated_spans = make_integrated_string(text)
+    integrated_dubiouses = detect_bad_formed_integrated(integrated_string, integrated_spans, illegal_words)
+    # TODO cleanup false positive for integrated!
+    output = dubiouses_to_output(dubiouses + integrated_dubiouses)
     return output
 
 
@@ -198,4 +261,8 @@ def run_tests():
 
 
 if __name__ == '__main__':
-    run_tests()
+    # run_tests()
+    # print(get_persian_similar_characters())
+    print(edit_distance("آقا", "آبا"))
+    # print(re.match(ACCEPTABLE_PERSIAN_CHARS_REGEX, "!"))
+    # print(ACCEPTABLE_PERSIAN_CHARS_REGEX)
